da57831c2955c1a9ad6814af4cdc3119a513f02e
Yesim nur Akar <yesimnurakar@gmail.com>
2021-04-26 20:46:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,16 +11,5 @@ function moveDodgerLeft() {
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
-  else(e.key === "ArrowRight")
-   moveDodgerRight();
-
 });
 
-function moveDodgerRight() {
-  let rightNumbers = dodger.style.left.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
-
-  if (left > 0) {
-    dodger.style.left = `${left + 1}px`;
-  }
-}
