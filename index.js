ddcb0518c99b4025dfca0dab82e77d9504f99435
Yesim nur Akar <yesimnurakar@gmail.com>
2021-04-26 20:52:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ document.addEventListener("keydown", function(e) {
     moveDodgerLeft();
   }
 });
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
+
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`;
+  }
+}
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
