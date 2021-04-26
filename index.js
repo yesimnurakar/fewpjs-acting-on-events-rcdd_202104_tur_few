6284fa17ebc87778f1a2c02aff1fd2e2e11836ff
Yesim nur Akar <yesimnurakar@gmail.com>
2021-04-26 20:56:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ document.addEventListener("keydown", function(e) {
   }
 });
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
 
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`;
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
   }
 }
 document.addEventListener("keydown", function(e) {
